19593ef4f7bbf30d3254be8db1945690e86cb5b9
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-01-03 20:54:59 +0000

    added margin to tool bar

--- app/tasks/data-table-toolbar.tsx
+++ app/tasks/data-table-toolbar.tsx
@@ -37,7 +37,7 @@ export function DataTableToolbar<TData>({
   const isFiltered = table.getState().columnFilters.length > 0;
 
   return (
-    <div className="flex flex-1 items-center space-x-2">
+    <div className="flex flex-1 items-center space-x-2 mb-2">
       <Input
         placeholder="Filter tasks..."
         value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
