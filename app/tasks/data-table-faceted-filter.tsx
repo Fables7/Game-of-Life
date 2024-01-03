0c2dc845450e5c4446633ecf9e763315f9479f2f
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-01-03 11:28:29 +0000

    Installed popover from shadnc

--- app/tasks/data-table-faceted-filter.tsx
+++ app/tasks/data-table-faceted-filter.tsx
@@ -4,4 +4,12 @@ import { Column } from "@tanstack/react-table";
 import { cn } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
